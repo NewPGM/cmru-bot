587953e5987cd68396193025eacf35a801b3427a
Seerasak Tajai <64143109@g.cmru.ac.th>
2025-01-12 00:36:42 +0700

    dev

--- app.js
+++ app.js
@@ -8,11 +8,20 @@ dotenv.config();
 const app = express();
 app.use(cors());
 
-// Use the webhook routes
+// Root route
+app.get('/', (req, res) => {
+  res.send('Server is up and running!');
+});
+
+// Webhook route
 app.use('/webhook', webhookRoutes);
 
-const port = process.env.PORT || 7000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-//
+// Listen only if not running in Vercel dev
+if (!process.env.VERCEL) {
+  const port = process.env.PORT || 7000;
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
+
+module.exports = app; // สำคัญสำหรับ Vercel Dev
