8373896ac794ec7a1b6e26868097f5a1c7c153cd
Seerasak Tajai <64143109@g.cmru.ac.th>
2025-01-11 23:10:55 +0700

    ww

--- app.js
+++ app.js
@@ -7,12 +7,17 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
-// Use the webhook routes
+// Root route
+app.get('/', (req, res) => {
+  res.send('Server is up and running!');
+});
+
+// Webhook route
 app.use('/webhook', webhookRoutes);
 
 const port = process.env.PORT || 7000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-//
